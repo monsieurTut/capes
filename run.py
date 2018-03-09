a1ae829fa2d75edb1ae467feca2a301ca5e83ce0
Canelle <c.soum_all@outlook.fr>
2018-03-09 23:33:17 +0100

    implemented function orient

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from functions import *
 
 
-X = [0, 1, 2]
-Y = [ -1, 2 , 0]
+X = [0, 1, 1, 4, 4, 5, 5, 7, 7, 8, 11, 13]
+Y = [0, 4, 8, 1, 4, 9, 6, -1, 2, 5, 6, 1]
 tab = [X,Y]
 dessiner_points(tab)
-dessiner_Enveloppe(tab)
-show()
+#dessiner_Enveloppe(tab)
+print(plus_bas(tab))
+print (orient(tab,0,3,4))
+print (orient(tab,8,9,10))
+print (orient(tab,8,9,9))
 
