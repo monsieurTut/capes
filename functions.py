266c5d9af281b3a3eb37a16b841bf056a2f22e48
Adrien <derosiere@gmail.com>
2018-03-14 22:13:53 +0100

    code formatting

--- functions.py
+++ functions.py
@@ -1,33 +1,35 @@
 from matplotlib.pyplot import *
 
+
 def dessiner_points(tab):
     plot(tab[0], tab[1], 'ko')
-        
-def dessiner_Enveloppe(tab):
+
+
+def dessiner_enveloppe(tab):
     plot(tab[0] + [tab[0][0]], tab[1] + [tab[1][0]], 'k-')
 
+
 def plus_bas(tab):
-    mint =tab[1][0]
+    mint = tab[1][0]
     imin = 0
     for i, y in enumerate(tab[1]):
         if y < mint:
             imin, mint = i, y
         elif y == mint:
-            if tab[0][i]<tab[0][imin]:
+            if tab[0][i] < tab[0][imin]:
                 imin = i
     return imin
-    
-def orient (tab, i, j, k):
+
+
+def orient(tab, i, j, k):
     x1 = tab[0][i] - tab[0][j]
     x2 = tab[0][i] - tab[0][k]
     y1 = tab[1][i] - tab[1][j]
     y2 = tab[1][i] - tab[1][k]
-    
-    if (x1*y2 -x2*y1) > 0:
+
+    if (x1 * y2 - x2 * y1) > 0:
         return 1
-    elif (x1*y2 -x2*y1) < 0:
+    elif (x1 * y2 - x2 * y1) < 0:
         return -1
     else:
         return 0
-        
-                
